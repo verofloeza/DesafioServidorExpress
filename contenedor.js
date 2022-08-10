0921cde72da0ba0270edfea276e904d7ab85a1b5
Verónica <verofloeza@gmail.com>
2022-08-10 15:11:26 -0300

    Desafio Servidor Express

---
+++ contenedor.js
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+const fs = require("fs/promises");
+const path = "./archivos/database.json";
+class Contenedor{
+    constructor(){};
+    
+    async getAll(){
+        
+        try {
+            const objs = await fs.readFile(path, "utf-8");
+            return  JSON.parse(objs);
+        } catch (error) {
+            return error;
+        }
+    };
+
+    async getRandom(){
+        const objs = await fs.readFile(path, "utf-8");
+        let random = parseInt(Math.random() * JSON.parse(objs).length) + 1;
+        return JSON.parse(objs).find( product => product.id === random );
+
+    };
+};
+
+module.exports = Contenedor;
+=======
+const fs = require("fs/promises");
+const path = "./archivos/database.json";
+class Contenedor{
+    constructor(){};
+    
+    async getAll(){
+        
+        try {
+            const objs = await fs.readFile(path, "utf-8");
+            return  JSON.parse(objs);
+        } catch (error) {
+            return error;
+        }
+    };
+
+    async getRandom(){
+        const objs = await fs.readFile(path, "utf-8");
+        let random = parseInt(Math.random() * JSON.parse(objs).length) + 1;
+        return JSON.parse(objs).find( product => product.id === random );
+
+    };
+};
+
+module.exports = Contenedor;
+>>>>>>> 33a1833c89ee26e8002ee731e97ead1789dd028d
---
+++ server.js
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+const express = require("express");
+const Contenedor = require("./contenedor.js");
+
+const app= express();
+const productos = new Contenedor();
+
+
+app.get("/", (request, responde) =>{ 
+    responde.send("servidor iniciado con express")
+});
+app.get("/productos", async (request, responde) =>{ 
+    let pro = await productos.getAll();
+    responde.send(pro);
+});
+app.get("/productosRandom", async (request, responde) =>{ 
+    let Ran = await productos.getRandom();
+    responde.send(Ran);
+});
+
+
+const server = app.listen(8080, ()=> {
+    console.log("servidor escuchando http://localhost:8080/")
+=======
+const express = require("express");
+const Contenedor = require("./contenedor.js");
+
+const app= express();
+const productos = new Contenedor();
+
+
+app.get("/", (request, responde) =>{ 
+    responde.send("servidor iniciado con express")
+});
+app.get("/productos", async (request, responde) =>{ 
+    let pro = await productos.getAll();
+    responde.send(pro);
+});
+app.get("/productosRandom", async (request, responde) =>{ 
+    let Ran = await productos.getRandom();
+    responde.send(Ran);
+});
+
+
+const server = app.listen(8080, ()=> {
+    console.log("servidor escuchando http://localhost:8080/")
+>>>>>>> 33a1833c89ee26e8002ee731e97ead1789dd028d
+});
